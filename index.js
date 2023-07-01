56bae8b65a81ed23be8a65943057f11961c790bf
pablo128482 <ashley.c.clay@gmail.com>
2023-06-30 18:43:57 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide() {
+    return 2000 / 100;
+    
+}
+    function square(number, parameter) {
+    const square = (parameter1) => parameter1 * parameter1;
+    let parameter1 = 2;
+    return square(2) //=> 4;
+  
+   }
+
+   function add(number, parameter){
+    const add = (parameter1, parameter2) => parameter1 + parameter2;
+    let parameter1 = 2;
+    let parameter2 = 5;
+    return add(2,5); //=> 7
+   }
